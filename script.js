4d74baa2fbd974f1139eab4422348d437a18fe94
Chris Rice <chrisrice2442@yahoo.com>
2023-10-02 16:52:10 -0400

    Added pseodocode to help myself plan and break down what I need to do

---
+++ script.js
@@ -0,0 +1,36 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("Hey, you clicked the button!");
+  var length = Number(prompt("Enter ")
+
+
+
+
+
+  return "Generated Password will go here!";
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+//Incorporate prompts with each question:
+
+
+// 1. Prompt the user for password critera
+//  a. Password length 8 < 128
+//  b. Lowercase, uppercase, numbers, special characters
+// 2. Validate the input.
+// 3. Generate password based on criteria.
+
+// 4. Display generated the password on the page
