0894e3b43e8d81211a539aec3f2cdd5f00caa24b
Chris Rice <chrisrice2442@yahoo.com>
2023-10-03 15:50:00 -0400

    trying to add if/else and stuff. trying to get it to work

--- script.js
+++ script.js
@@ -1,61 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-
-}
-
-console.log(getRandomLower());
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-
-}
-
-console.log(getRandomUpper());
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-
-}
-
-console.log(getRandomNumber());
-
-function getRandomSymbol() {
-  var symbols = "!@#$%^&*()_-+=<>,./{}[]"
-  return symbols[Math.floor(Math.random() * symbols.length)];
-
-}
-
-console.log(getRandomSymbol());
-
 //prompts to the user
 function generatePassword() {
-  console.log("Hey, you clicked the button!");
+
   var passwordLength = prompt("How long would you like your password? \nIt may be between 8 and 128 characters.")
-  var uppercase = confirm("Would you like your password to contain uppercase characters? \nClick OK for Yes! \nCancel for No.");
+  var uppercase = confirm("Would you like your password to contain uppercase characters? \nClick OK for Yes! \nCancel for No.")
   var lowercase = confirm("Would you like your password to contain lowercase characters? \nClick OK for Yes! \nCancel for No.")
   var symbols = confirm("Would you like your password to contain symbols? \nClick OK for Yes! \nCancel for No.")
   var numbers = confirm("Would you like your password to contain numbers? \nClick OK for Yes! \nCancel for No.")
-  
+
 
   //declaring limits for the password
   var uppercaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var lowercaseLtter = "abcdefghijklmnopqrstuvwxyz"
-  var symbol = "!@#$%^&*()?.</>|=+:;,{}[]_-"
+  var lowercaseLetter = "abcdefghijklmnopqrstuvwxyz"
+  var symbolCharacters = "!@#$%^&*()?.</>|=+:;,{}[]_-"
   var numberCharacters = "1234567890"
 
-  //actual logic for computer to check
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("You must enter a number between 8 and 128 you silly goose!")
-    // return (generatePassword())
-  }
+  var password = "";
 
+  // if (passwordLength === null) {
+  //   alert("Goodbye!")
+  // } else {
 
+  // if (passwordLength < 8 || passwordLength > 128) {
+  //   alert("You must enter a number between 8 and 128 you silly goose!")
+  // }}
 
-  return "Generated Password will go here!";
-}
+  for (var i = 0; i < length; i++) {
+    var charSet = "";
+
+    if (uppercase) {
+      charSet += uppercaseLetter;
+    } 
+    if (lowercase) {
+      charSet += lowercaseLetter;
+    }
+    if (symbols) {
+      charSet += symbolCharacters
+    }
+    if (numbers) {
+      charSet += numberCharacters
+    }
+}}
+
+var randomIndex = Math.floor(Math.random() * charSet.length)
+var randomChar = charSet.charAt(randomIndex);
+
+password += randomChar;
 
 // Write password to the #password input
 function writePassword() {
@@ -67,15 +59,3 @@ function writePassword() {
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-//Incorporate prompts with each question:
-
-
-// 1. Prompt the user for password critera
-//  a. Password length 8 < 128
-//  b. Lowercase, uppercase, numbers, special characters
-// 2. Validate the input.
-// 3. Generate password based on criteria.
-
-// 4. Display generated the password on the page
