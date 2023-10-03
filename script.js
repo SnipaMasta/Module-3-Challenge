32bda42615b75137ba37a023adca011b5245267e
Chris Rice <chrisrice2442@yahoo.com>
2023-10-03 13:31:36 -0400

    Added console.logs to retrieve random upper, lowercase, symbols, and numbers using char code function, math.random, and math.floor

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+
+}
+
+console.log(getRandomLower());
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+
+}
+
+console.log(getRandomUpper());
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+
+}
+
+console.log(getRandomNumber());
+
+function getRandomSymbol() {
+  var symbols = "!@#$%^&*()_-+=<>,./{}[]"
+  return symbols[Math.floor(Math.random() * symbols.length)];
+
+}
+
+console.log(getRandomSymbol());
+
 //prompts to the user
 function generatePassword() {
   console.log("Hey, you clicked the button!");
-  var uppercase = confirm("Would you like your password to contain uppercase characters? Click OK for Yes! Cancel for No.");
-  var lowercase = confirm("Would you like your password to contain lowercase characters? Click OK for Yes! Cancel for No.")
-  var symbols = confirm("Would you like your password to contain symbols? Click OK for Yes! Cancel for No.")
-  var numbers = confirm("Would you like your password to contain numbers? Click OK for Yes! Cancel for No.")
-  var passwordLength = prompt("How long would you like your password? It may be between 8 and 128 characters.")
+  var passwordLength = prompt("How long would you like your password? \nIt may be between 8 and 128 characters.")
+  var uppercase = confirm("Would you like your password to contain uppercase characters? \nClick OK for Yes! \nCancel for No.");
+  var lowercase = confirm("Would you like your password to contain lowercase characters? \nClick OK for Yes! \nCancel for No.")
+  var symbols = confirm("Would you like your password to contain symbols? \nClick OK for Yes! \nCancel for No.")
+  var numbers = confirm("Would you like your password to contain numbers? \nClick OK for Yes! \nCancel for No.")
+  
 
   //declaring limits for the password
   var uppercaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -17,8 +47,10 @@ function generatePassword() {
   var numberCharacters = "1234567890"
 
   //actual logic for computer to check
-
-  
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("You must enter a number between 8 and 128 you silly goose!")
+    // return (generatePassword())
+  }
 
 
 
