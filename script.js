217441f7dea826589ba1508e0075af2e9685a7df
Chris Rice <chrisrice2442@yahoo.com>
2023-10-04 14:01:28 -0400

    cleaned up the code, strippped to bare essentials and began adding if statements with console logs

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
   var passwordLength = prompt("How long would you like your password? \nIt may be between 8 and 128 characters.")
+  console.log(passwordLength);
   var uppercase = confirm("Would you like your password to contain uppercase characters? \nClick OK for Yes! \nCancel for No.")
+  console.log(uppercase);
   var lowercase = confirm("Would you like your password to contain lowercase characters? \nClick OK for Yes! \nCancel for No.")
+  console.log(lowercase);
   var symbols = confirm("Would you like your password to contain symbols? \nClick OK for Yes! \nCancel for No.")
+  console.log(symbols);
   var numbers = confirm("Would you like your password to contain numbers? \nClick OK for Yes! \nCancel for No.")
-
+  console.log(numbers);
 
   //declaring limits for the password
   var uppercaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -18,36 +22,19 @@ function generatePassword() {
   var numberCharacters = "1234567890"
 
   var password = "";
+  if (symbols) {
+    password = password + symbols 
+  }
+  if (numbers) {
+    password = password + numbers 
+  } if (uppercase) {
+    password = password + uppercase 
+  } if (lowercase) {
+    password = password + lowercase 
+  }
+  }
+  console.log(password);
 
-  if (passwordLength === NaN) {
-    alert("Goodbye!") }
-  // } else {
-
-  // if (passwordLength < 8 || passwordLength > 128) {
-  //   alert("You must enter a number between 8 and 128 you silly goose!")
-  // }}
-var charSet = "";
-  for (var i = 0; i < length; i++) {
-    var charSet = "";
-
-    if (uppercase) {
-      charSet += uppercaseLetter;
-    } 
-    if (lowercase) {
-      charSet += lowercaseLetter;
-    }
-    if (symbols) {
-      charSet += symbolCharacters
-    }
-    if (numbers) {
-      charSet += numberCharacters
-    }
-}}
-
-// var randomIndex = Math.floor(Math.random() * charSet.length)
-// var randomChar = charSet.charAt(randomIndex);
-
-// password += randomChar;
 
 // Write password to the #password input
 function writePassword() {
