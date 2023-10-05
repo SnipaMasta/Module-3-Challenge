08010e07b04ec1f73ee682172fcb05cd431d1df6
Chris Rice <chrisrice2442@yahoo.com>
2023-10-05 16:50:56 -0400

    Got it to actually work and put the password in teh box. trying to figure out why the conditions aren't quite working properly

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-//prompts to the user
+//Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 function generatePassword() {
-
+  var uppercaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowercaseLetter = "abcdefghijklmnopqrstuvwxyz"
+  var symbolCharacters = "!@#$%^&*()?.</>|=+:;,{}[]_-"
+  var numberCharacters = "1234567890"
   var passwordLength = prompt("How long would you like your password? \nIt may be between 8 and 128 characters.")
   console.log(passwordLength);
   var uppercase = confirm("Would you like your password to contain uppercase characters? \nClick OK for Yes! \nCancel for No.")
@@ -15,34 +22,32 @@ function generatePassword() {
   var numbers = confirm("Would you like your password to contain numbers? \nClick OK for Yes! \nCancel for No.")
   console.log(numbers);
 
-  //declaring limits for the password
-  var uppercaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var lowercaseLetter = "abcdefghijklmnopqrstuvwxyz"
-  var symbolCharacters = "!@#$%^&*()?.</>|=+:;,{}[]_-"
-  var numberCharacters = "1234567890"
 
-  var password = "";
+
+  //actually pulling password characters based on input 
+
+  var variables = ""
   if (symbols) {
-    password = password + symbols 
+    variables = password + symbolCharacters
   }
   if (numbers) {
-    password = password + numbers 
-  } if (uppercase) {
-    password = password + uppercase 
-  } if (lowercase) {
-    password = password + lowercase 
+    variables = password + numberCharacters
   }
+  if (uppercase) {
+    variables = password + uppercaseLetter
   }
-  console.log(password);
+  if (lowercase) {
+    variables = password + lowercaseLetter
+  }
 
+  console.log(variables);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var newPass = ""
+    for (let i = 0; i < passwordLength; i++) {
+      newPass = newPass + variables.charAt(Math.floor(Math.random() * variables.length))
+    }
+    console.log(variables);
+    return newPass //generated password.
+  }
 
-  passwordText.value = password;
-
-}
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
