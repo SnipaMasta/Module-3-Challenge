f8f045028ab12688d001c7beaf4e2ed2ac605617
Chris Rice <chrisrice2442@yahoo.com>
2023-10-05 22:47:26 -0400

    WORKS. i had a wrong variable in my if statements

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ function generatePassword() {
   var numberCharacters = "1234567890"
   var passwordLength = prompt("How long would you like your password? \nIt may be between 8 and 128 characters.")
   console.log(passwordLength);
+  if (passwordLength === null || passwordLength === "") {
+    alert("Try again")
+    return;
+  }
   var uppercase = confirm("Would you like your password to contain uppercase characters? \nClick OK for Yes! \nCancel for No.")
   console.log(uppercase);
   var lowercase = confirm("Would you like your password to contain lowercase characters? \nClick OK for Yes! \nCancel for No.")
@@ -23,31 +27,28 @@ function generatePassword() {
   console.log(numbers);
 
 
-
   //actually pulling password characters based on input 
 
   var variables = ""
   if (symbols) {
-    variables = password + symbolCharacters
+    variables = variables + symbolCharacters
   }
   if (numbers) {
-    variables = password + numberCharacters
+    variables = variables + numberCharacters
   }
   if (uppercase) {
-    variables = password + uppercaseLetter
+    variables = variables + uppercaseLetter
   }
   if (lowercase) {
-    variables = password + lowercaseLetter
+    variables = variables + lowercaseLetter
   }
 
   console.log(variables);
 
-    var newPass = ""
-    for (let i = 0; i < passwordLength; i++) {
-      newPass = newPass + variables.charAt(Math.floor(Math.random() * variables.length))
-    }
-    console.log(variables);
-    return newPass //generated password.
+  var newPass = ""
+  for (let i = 0; i < passwordLength; i++) {
+    newPass = newPass + variables.charAt(Math.floor(Math.random() * variables.length))
   }
-
+  return newPass //generated password.
+}
 generateBtn.addEventListener("click", writePassword);
